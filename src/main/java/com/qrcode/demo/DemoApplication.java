6d45b6f1928c9aa87513b69b46c831821ad02f8b
bpolaswar <bhumesh.polaswar@ployglots.io>
2021-10-02 09:25:17 +0530

    added method to generate QR Code

--- src/main/java/com/qrcode/demo/DemoApplication.java
+++ src/main/java/com/qrcode/demo/DemoApplication.java
@@ -9,22 +9,33 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.Date;
 import java.util.Hashtable;
 
 @SpringBootApplication
 public class DemoApplication
 {
 	public static void main(String[] args) throws Exception {
-
 		SpringApplication.run(DemoApplication.class, args);
+		String fileName ="qrCode_"+new Date().getTime() +".png";
+		RequestBody requestBody = RequestBody.builder().QrCodeText("Dummy QR Code text").build();
+		String pathOfQrImage = generateQRCode(requestBody.getQrCodeText(), 350, 350, fileName);
+		System.out.print("pathOfQrImage : "+ pathOfQrImage);
 	}
 
-	
-
+	private static String generateQRCode(String text, int width, int height, String filePath) throws WriterException, IOException
+	{
+		QRCodeWriter writer = new QRCodeWriter();
+		BitMatrix bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, width, height);
+		Path path = FileSystems.getDefault().getPath(filePath);
+		MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+		return path.toAbsolutePath().toString();
+	}
 }
