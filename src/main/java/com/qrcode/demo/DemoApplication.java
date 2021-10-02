230f8119acaefc26cd54e3f6c262d0d6faeb5d33
yShukla8788 <72255731+yShukla8788@users.noreply.github.com>
2021-10-02 09:07:40 +0530

    initial commit

---
+++ src/main/java/com/qrcode/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.qrcode.demo;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Hashtable;
+
+@SpringBootApplication
+public class DemoApplication
+{
+	public static void main(String[] args) throws Exception {
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	
+
+}
